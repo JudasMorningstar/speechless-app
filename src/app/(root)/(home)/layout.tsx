346b500d471e7ad8f79e8fe256210a364b0b806a
JunadeMorningstar <junade.dev@gmail.com>
2024-03-30 23:59:59 +0200

    added sidebar links

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import SidebarNav from "@/components/Sidebar";
 
+
 import { sidebarConfig } from "@/config";
 import React from "react";
 
@@ -9,7 +10,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
     <main className="relative">
       <Navbar />
       <div className="flex">
-        <SidebarNav items={sidebarConfig.sidebarNav} className="p-1" />
+        <SidebarNav items={sidebarConfig.sidebarNav} />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,40 +13,49 @@ export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 const SidebarNav = ({ items, className, ...props }: SidebarNavProps) => {
   const pathname = usePathname();
-  //   if (!items?.length) return null;
-  return (
-    <section className="sticky left-0 h-screen top-0 flex w-fit flex-col justify-between bg-border p-6 pt-28 max-sm:hidden lg-w[264px] ">
-      <div className="flex flex- flex-col gap-6">
-        {items.map((link, index) => {
-          const Icon = link.icon ? Icons[link.icon] : null;
-          const isActive =
-            pathname === link.href || pathname?.startsWith(link.href as string);
-          return link.href ? (
-            <Link
-              href={link.href as string}
-              key={index}
-              className={cn(
-                "flex items-center gap-4 p-4 rounded-lg justify-start",
-                {
-                  "bg-primary": isActive,
-                }
-              )}
-            >
-              {Icon && <Icon />}
-              <span>{link.title}</span>
-            </Link>
-          ) : (
-            <span
-              key={index}
-              className="flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline"
-            >
-              {link.title}
-            </span>
-          );
-        })}
-      </div>
-    </section>
-  );
+    if (!items?.length) return null;
+    // const segment = useSelectedLayoutSegment();
+
+    if (!items?.length) return null;
+
+    return (
+      <section className="sticky left-0 h-screen top-0 flex w-fit flex-col justify-between bg-border p-6 pt-28 max-sm:hidden lg-w[264px] ">
+        <div className="flex flex- flex-col gap-6">
+          {items.map((item, index) => {
+            const Icon = item.icon ? Icons[item.icon] : null;
+            const isActive = pathname === item.href;
+            //   ||
+            //   pathname.startsWith(item.href as string);
+
+            return item.href ? (
+              <Link
+                aria-label={item.title}
+                key={index}
+                href={item.href}
+                target={item.external ? "_blank" : ""}
+                rel={item.external ? "noreferrer" : ""}
+                className={cn(
+                  "flex items-center gap-4 p-4 rounded-lg justify-start",
+                  {
+                    "bg-primary": isActive,
+                  }
+                )}
+              >
+                {Icon && <Icon aria-hidden="true" />}
+                <span>{item.title}</span>
+              </Link>
+            ) : (
+              <span
+                key={index}
+                className="flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline"
+              >
+                {item.title}
+              </span>
+            );
+          })}
+        </div>
+      </section>
+    );
 };
 
 export default SidebarNav;
