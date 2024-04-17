713bcb43d96e58ebc8182e4346e73b8a47795dc8
JunadeMorningstar <junade.dev@gmail.com>
2024-04-18 01:12:53 +0200

    feat: added video setup

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -3,23 +3,45 @@
 import Loader from "@/components/loader";
 import MeetingRoom from "@/components/meeting/MeetingRoom";
 import MeetingSetup from "@/components/meeting/MeetingSetup";
+import { Alert } from "@/components/ui/alert";
 import { useGetCallByID } from "@/hooks/useGetCallByID";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import React from "react";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
 
-const Meeting = ({ params: { id } }: { params: { id: string } }) => {
-  const { user, isLoaded } = useUser();
-  const [isSetupComplete, setIsSetupComplete] = React.useState(false);
+const Meeting = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
   const { call, isCallLoading } = useGetCallByID(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <Loader />;
 
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed =
+    call.type === "invited" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
+
   return (
-    <main className="h-screen w-full ">
+    <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
--- src/components/meeting/MeetingSetup.tsx
+++ src/components/meeting/MeetingSetup.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+"use client";
 
-const MeetingSetup = () => {
-  return <div>MeetingSetup</div>;
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+
+import { Alert } from "../ui/alert";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import { Checkbox } from "../ui/checkbox";
+
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  // https://getstream.io/video/docs/react/guides/call-and-participant-state/#call-state
+  const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+  const callStartsAt = useCallStartsAt();
+  const callEndedAt = useCallEndedAt();
+  const callTimeNotArrived =
+    callStartsAt && new Date(callStartsAt) > new Date();
+  const callHasEnded = !!callEndedAt;
+
+  const call = useCall();
+
+  if (!call) {
+    throw new Error(
+      "useStreamCall must be used within a StreamCall component."
+    );
+  }
+
+  // https://getstream.io/video/docs/react/ui-cookbook/replacing-call-controls/
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+
+  useEffect(() => {
+    if (isMicCamToggled) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [isMicCamToggled, call.camera, call.microphone]);
+
+  if (callTimeNotArrived)
+    return (
+      <Alert
+        title={`Your Meeting has not started yet. It is scheduled for ${callStartsAt.toLocaleString()}`}
+      />
+    );
+
+  if (callHasEnded)
+    return (
+      <Alert
+        title="The call has been ended by the host"
+        // iconUrl="/icons/call-ended.svg"
+      />
+    );
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-center text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <Checkbox
+          id="mic-cam-toggle"
+          onClick={() => setIsMicCamToggled(!isMicCamToggled)}
+        />
+        <Label
+          htmlFor="mic-cam-toggle"
+          className="flex items-center justify-center gap-2 font-medium"
+        >
+          Join with mic and camera off
+        </Label>
+
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-primary px-4 py-2.5"
+        onClick={() => {
+          call.join();
+
+          setIsSetupComplete(true);
+        }}
+      >
+        Join meeting
+      </Button>
+    </div>
+  );
 };
 
 export default MeetingSetup;
