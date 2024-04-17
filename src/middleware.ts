559fb296a1764943b89bd450e2509a19e01bce4f
JunadeMorningstar <junade.dev@gmail.com>
2024-04-17 20:15:28 +0200

    fix: fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/upcoming",
+    "/previous",
+    "/recordings",
+    "/personal-room",
+    "/(api|trpc)(.*)",
+  ],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
